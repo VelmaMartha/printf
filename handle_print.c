4d81dca8bd7c6cfb122f2af83a416d0b285de5b1
VelmaMartha <marthaauma3508@gmail.com>
2023-03-28 14:54:52 -0700

    non-custom conversion specifiers

--- handle_print.c
+++ handle_print.c
@@ -3,8 +3,8 @@
  * handle_print - Prints an argument based on it's type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * @ind: ind
+ * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
